437e3adcd48cea82a53ecce3fbbf32853f87bdbe
Austin Rollins <atrollins42@students.tntech.edu>
2019-11-05 19:37:22 -0600

    Linked dependent libraries so that the library can be installed eaiser and faster.

--- Anemone/Anemone.h
+++ Anemone/Anemone.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#ifdef ANEMONE_PATH
+#pragma comment(lib, ANEMONE_PATH + "Anemone/lib/OpenGL32.lib")
+#pragma comment(lib, ANEMONE_PATH + "Anemone/lib/glew/x86/glew32s.lib")
+#pragma comment(lib, ANEMONE_PATH + "Anemone/lib/glfw3.lib")
+#pragma comment(lib, ANEMONE_PATH + "Anemone/lib/Project Anemone.lib")
+#else
+#pragma comment(lib, "Anemone/lib/OpenGL32.lib")
+#pragma comment(lib, "Anemone/lib/glew/x86/glew32s.lib")
+#pragma comment(lib, "Anemone/lib/glfw3.lib")
+#pragma comment(lib, "Anemone/lib/Project Anemone.lib")
+#endif
+
 namespace Anemone
 {
 
