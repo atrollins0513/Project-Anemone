e24209e55afcb24d6863cac26dc7611d4e4693c2
Austin Rollins <atrollins42@students.tntech.edu>
2020-05-25 18:56:17 -0500

    Added explode function.

--- Anemone/Utilities.h
+++ Anemone/Utilities.h
@@ -2,10 +2,14 @@
 
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <utility>
+#include <vector>
 
 #include "Error.h"
 
 namespace Anemone
 {
 	extern std::string LoadFile(std::string filename, int file_mode = std::ios::in);
+	extern std::vector<std::string> explode(std::string const& s, char delim);
 };
--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -37,4 +37,17 @@ namespace Anemone
 			return "";
 		}
 	}
+
+	std::vector<std::string> explode(std::string const& s, char delim)
+	{
+		std::vector<std::string> result;
+		std::istringstream iss(s);
+
+		for (std::string token; std::getline(iss, token, delim); )
+		{
+			result.push_back(std::move(token));
+		}
+
+		return result;
+	}
 };
